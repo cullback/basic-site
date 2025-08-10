aa879644e7487bec2b491c73d3e75daace3d1c60
cullback <cullback@fastmail.com>
2025-08-10 13:43:16 -0400

    Add server_time api route

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,11 @@
 //! API routes.
-use axum::Router;
+use axum::{Router, routing::get};
 
 use crate::app_state::AppState;
 
+mod server_time;
+
 pub fn router() -> Router<AppState> {
     Router::new()
+        .route("/time", get(server_time::get))
 }
