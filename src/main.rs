aaa7f2fca09f49911a4ae11fd1297ada9908ccdc
cullback <cullback@fastmail.com>
2025-09-18 04:21:39 +0000

    Move clippy settings to Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-#![warn(
-    clippy::all,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::cargo,
-    clippy::restriction
-)]
-#![allow(
-    clippy::allow_attributes_without_reason,
-    clippy::arbitrary_source_item_ordering,
-    clippy::blanket_clippy_restriction_lints,
-    clippy::cargo_common_metadata,
-    clippy::expect_used,
-    clippy::implicit_return,
-    clippy::missing_docs_in_private_items,
-    clippy::mod_module_files,
-    clippy::multiple_crate_versions,
-    clippy::print_stdout,
-    clippy::question_mark_used,
-    clippy::single_call_fn,
-    clippy::single_char_lifetime_names,
-    clippy::std_instead_of_core,
-    clippy::unwrap_used
-)]
 use std::net::SocketAddr;
 
 use axum::Router;
