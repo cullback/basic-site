95abbef8239654cc3ee41bd6d555780ef32bd7f6
cullback <cullback@fastmail.com>
2025-09-19 21:29:46 +0000

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      fenix,
-      ...
-    }:
+    { self, nixpkgs }:
     let
       system = "aarch64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-
-      rustToolchain = fenix.packages.${system}.stable.toolchain;
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          rustToolchain
+          cargo
+          rustc
+          clippy
+          rust-analyzer
+          rustfmt
+          sqlx-cli
           openssl
           pkg-config
-          sqlx-cli
         ];
-
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        OPENSSL_NO_VENDOR = 1;
       };
     };
 }
